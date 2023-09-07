dd885cbd64b282af3dcd37dfaf130c83a232f705
komala-sts <143262044+komala-sts@users.noreply.github.com>
2023-09-07 17:14:57 +0100

    Add files via upload

---
+++ MotivationEmail/Motivation_email_Project.py
@@ -0,0 +1,27 @@
+#Monday Motivation Project
+import smtplib
+import datetime as dt
+import calendar
+import random
+
+
+MY_EMAIL = "komalariyadh@gmail.com"
+MY_PASSWORD = "uemltjqclrekwapl"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+dayname= calendar.day_name[weekday]
+with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+#print(quote) 
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    connection.starttls()
+    connection.login(MY_EMAIL, MY_PASSWORD)
+    connection.sendmail(
+        from_addr=MY_EMAIL,
+        to_addrs=["komalasts@gmail.com", "shiv.pcs@gmail.com", "pinkyjosh2@gmail.com"],
+        msg=f"Subject:{dayname} Motivation Quote from Komal through PythonAnyWhere.com\n\n{quote}"
+    )
+
